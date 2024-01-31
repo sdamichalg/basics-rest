550200c656ab8466487ca58faae7a2b3f1984eeb
Michal Gebski <gebski.michal@gmail.com>
2024-01-31 18:58:25 +0100

    7. @Qualifier

--- src/main/java/pl/sda/micgeb/spring_rest_app/controller/commercial/CommercialController.java
+++ src/main/java/pl/sda/micgeb/spring_rest_app/controller/commercial/CommercialController.java
@@ -1,6 +1,7 @@
 package pl.sda.micgeb.spring_rest_app.controller.commercial;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +11,13 @@ import pl.sda.micgeb.spring_rest_app.service.commercial.CommercialService;
 
 @RestController
 @RequestMapping("/commercial")
-@RequiredArgsConstructor
 public class CommercialController {
 
     private final CommercialService commercialService;
+
+    public CommercialController(@Qualifier("businessCustomerCommercialService") CommercialService commercialService) {
+        this.commercialService = commercialService;
+    }
 
     @PostMapping
     public ResponseEntity<Void> sendCommercial(@RequestBody String content) {
--- src/main/java/pl/sda/micgeb/spring_rest_app/service/commercial/DefaultCustomerCommercialService.java
+++ src/main/java/pl/sda/micgeb/spring_rest_app/service/commercial/DefaultCustomerCommercialService.java
@@ -1,13 +1,11 @@
 package pl.sda.micgeb.spring_rest_app.service.commercial;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@Primary
-public class DefaultCustomerCommercialService implements CommercialService{
+public class DefaultCustomerCommercialService implements CommercialService {
     @Override
     public void sendCommercialOffer(String commercialContent) {
         log.info("It's an offer for default customers: " + commercialContent);
